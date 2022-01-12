3347828958835aa02d6fee5a737e9426147de313
Lukas Spitznagel <lukas.spitznagel@netzint.de>
2022-01-12 09:46:47 +0100

    add daemonHandler and remove customerdata

--- daemonHandler.sh
+++ daemonHandler.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+case "$1" in
+  start)
+	docker-compose up -d
+    ;;
+  stop)
+	docker-compose down
+	docker-compose rm
+	;;
+  restart)
+	docker-compose down
+	docker-compose rm
+    docker-compose up -d
+    ;;
+  *)
+	echo "$1 not supported"
+	echo "use start or stop"
+	;;
+esac
